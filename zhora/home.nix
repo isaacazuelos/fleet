a8319af3e7335f47032a6b4cde4a3cab0bd65fa9
Isaac Azuelos <isaac@azuelos.ca>
2026-04-11 20:11:15 -0600

    Get `nix develop` and `nix shell` to use fish on macos, with bad hacks.

--- zhora/home.nix
+++ zhora/home.nix
@@ -19,6 +19,23 @@
       enable = true;
       shellInit = ''
         set -gx PATH $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin $PATH
+
+        # HACK: nix-your-shell is only ad-hoc signed and gets killed by macOS.
+        # It's not on Homebrew either, so we can't easily get a signed build.
+        # Instead, we do roughly what `nix-your-shell fish | source` does, just
+        # a little worse.
+        function nix
+            if test (count $argv) -gt 0
+                switch $argv[1]
+                case develop shell run
+                    command nix $argv --command /opt/homebrew/bin/fish
+                case '*'
+                    command nix $argv
+                end
+            else
+                command nix $argv
+            end
+        end
       '';
     };
     ghostty = {
@@ -29,10 +46,10 @@
         font-family = "Berkeley Mono";
         font-size = 14;
         command = "/opt/homebrew/bin/fish --login --interactive";
-	window-padding-x = 16;
-	window-padding-y = 16;
-	background-opacity = 0.75;
-	background-blur = "macos-glass-regular";
+	      window-padding-x = 16;
+	      window-padding-y = 16;
+	      background-opacity = 0.75;
+	      background-blur = "macos-glass-regular";
       };
     };
     git = {
